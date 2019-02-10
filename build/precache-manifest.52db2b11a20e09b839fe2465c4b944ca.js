4f6364d25e38304872eeaeea57afef3f85a80881
Jetro Hulkko <jetro.hulkko@helsinki.fi>
2019-02-10 16:30:37 +0200

    frontend works?

---
+++ build/precache-manifest.52db2b11a20e09b839fe2465c4b944ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1f9b7ce95484235bebc",
+    "url": "/static/css/main.efebd688.chunk.css"
+  },
+  {
+    "revision": "a1f9b7ce95484235bebc",
+    "url": "/static/js/main.a1f9b7ce.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eead04ffaac7d2eb0aa0d2da1b31aa5f",
+    "url": "/index.html"
+  }
+];
